f6049e366cb17e8277b54714df642624d19a010f
WeiLai5432 <543256949@qq.com>
2018-02-01 12:25:35 +0800

    assignment2 first commit

--- assignment2starterCode/CompliantNode.java
+++ assignment2starterCode/CompliantNode.java
@@ -1,26 +1,48 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
 
+    double p_graph, p_malicious, p_txDistribution;
+
+    int numRounds;
+
+    boolean[] followees;
+
+    Set<Transaction> pendingTransactions;
+
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
+        this.p_graph = p_graph;
+        this.p_malicious = p_malicious;
+        this.p_txDistribution = p_txDistribution;
+        this.numRounds = numRounds;
     }
 
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
+        this.followees = followees;
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
         // IMPLEMENT THIS
+        this.pendingTransactions = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+        return this.pendingTransactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
+        Set<Transaction> pdTransactions = new HashSet<>();
+        for (Candidate can : candidates){
+            Transaction tx = can.tx;
+            pdTransactions.add(tx);
+        }
+        this.setPendingTransaction(pdTransactions);
     }
 }
